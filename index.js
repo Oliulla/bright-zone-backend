3e5ecb22bf9c7f9deaf69e5a0e5708301386318b
Oliulla <razasm29@gmail.com>
2022-10-27 02:25:03 +0600

    made single course api

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const app = express();
 app.use(cors());
 const Port = process.env.PORT || 5000;
 
+const courses = require("./data/courses.json");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(courses)
 })
+
+app.get('/course/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(c => c.id === id);
+  //   console.log(course)
+    res.send(course)
+  })
 
 app.listen(Port, () => {
   console.log(`Course server running on ${Port}`)
